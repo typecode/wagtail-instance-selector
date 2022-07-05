2c0f4b6d454b09a7fd8d39ffbb3e0bafc9819734
Mng <50384638+Mng-dev-ai@users.noreply.github.com>
2022-07-05 21:44:16 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import instance_selector
 
 
-install_requires = ["wagtail>=2.15"]
+install_requires = ["django==3.2.13","wagtail>=2.15"]
 
 setup(
     name="wagtail-instance-selector",
@@ -10,10 +10,10 @@ setup(
     packages=["instance_selector"],
     include_package_data=True,
     description="A widget for Wagtail's admin that allows you to create and select related items",
-    long_description="Documentation at https://github.com/ixc/wagtail-instance-selector",
+    long_description="Documentation at https://github.com/typecode/wagtail-instance-selector",
     author="The Interaction Consortium",
     author_email="admins@interaction.net.au",
-    url="https://github.com/ixc/wagtail-instance-selector",
+    url="https://github.com/typecode/wagtail-instance-selector",
     install_requires=install_requires,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
